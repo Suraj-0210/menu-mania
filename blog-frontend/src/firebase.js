8ab5eb6d3fc7dcd90c53d2854fff2f2764602db2
Suryakanta Prusty <kantaprustys@gmail.com>
2024-09-07 23:00:49 +0530

    added oauth functionality

---
+++ blog-frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "mern-blog-4cc1b.firebaseapp.com",
+  projectId: "mern-blog-4cc1b",
+  storageBucket: "mern-blog-4cc1b.appspot.com",
+  messagingSenderId: "434023813384",
+  appId: "1:434023813384:web:c4fb8fc5642f5a2fec9cbd",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- blog-frontend/src/pages/SignIn.jsx
+++ blog-frontend/src/pages/SignIn.jsx
@@ -7,6 +7,7 @@ import {
   signInStart,
   signInSuccess,
 } from "../redux/User/userSlice";
+import OAuth from "../components/oauth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -101,6 +102,7 @@ export default function SignIn() {
                 "SignIn"
               )}
             </Button>
+            <OAuth />
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Dont Have an account?</span>
