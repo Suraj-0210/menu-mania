e55801e292e56c29899733dd7f1e78d955f85b2e
Suryakanta Prusty <kantaprustys@gmail.com>
2024-09-10 21:40:51 +0530

    Update user api route

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,3 +1,64 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
 export const test = (req, res) => {
   res.json({ message: "Api is working" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userid) {
+    return next(errorHandler(403, "You are not allowed to update this user."));
+  }
+
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(
+        errorHandler(400, "Password cann't be less than 6 characters")
+      );
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, "Username must be between 7 and 20 characters")
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username cann't contain spaces"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "Username must be lowercase"));
+    }
+    if (!req.body.username.match(/^[a-z0-9]+$/)) {
+      return next(
+        errorHandler(400, "Username can only contain letters and numbers")
+      );
+    }
+
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.userid,
+        {
+          $set: {
+            username: req.body.username,
+            password: req.body.password,
+            profilePicture: req.body.profilePicture,
+            email: req.body.email,
+          },
+        },
+        {
+          new: true,
+        }
+      );
+
+      const { password, ...rest } = updatedUser._doc;
+
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  }
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { test } from "../controller/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
+import { updateUser } from "../controller/user.controller.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+router.put("/update/:userid", verifyToken, updateUser);
 
 export default router;
