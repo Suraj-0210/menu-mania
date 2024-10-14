22f757712083f18762223e8ffcece9d476f9af1b
Suryakanta Prusty <kantaprustys@gmail.com>
2024-10-14 18:25:32 +0530

    minor update

--- blog-frontend/src/components/Header.jsx
+++ blog-frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice.js";
-import { signoutSuccess } from "../redux/user/userSlice";
+import { signoutSuccess } from "../redux/user/userSlice.js";
 
 function Header() {
   const path = useLocation().pathname;
