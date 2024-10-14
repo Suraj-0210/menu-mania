d2084e527b325439b632763542f0eaadc69b625b
Suryakanta Prusty <kantaprustys@gmail.com>
2024-10-14 21:51:13 +0530

    test

--- blog-frontend/src/redux/theme/themeSlice.js
+++ blog-frontend/src/redux/theme/themeSlice.js
@@ -14,5 +14,5 @@ const themeSlice = createSlice({
   },
 });
 
-export const toggleTheme = themeSlice.actions;
+export const { toggleTheme } = themeSlice.actions;
 export default themeSlice.reducer;
