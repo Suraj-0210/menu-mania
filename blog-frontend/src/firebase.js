0553212fa69d1cbdcdd0e366ceb28475b5875d04
Suryakanta Prusty <kantaprustys@gmail.com>
2024-10-15 23:22:17 +0530

    changes in firbase.js

--- blog-frontend/src/firebase.js
+++ blog-frontend/src/firebase.js
@@ -1,11 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import process from "dotenv";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  apiKey: process.env.VITE_FIREBASE_APIKEY,
   authDomain: "mern-blog-4cc1b.firebaseapp.com",
   projectId: "mern-blog-4cc1b",
   storageBucket: "gs://mern-blog-4cc1b.appspot.com",
